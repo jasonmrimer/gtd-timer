b239834805925bad818708fee1e13ae35adff9ab
Engineer <jasonmrimer@gmail.com>
2021-04-04 15:15:27 -0600

    functional connection pooling

---
+++ controllers/src/main/java/edu/louisville/cse640/rimer/controllers/TimerController.java
@@ -0,0 +1,52 @@
+package edu.louisville.cse640.rimer.controllers;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TimerController {
+  private final Connection connection;
+
+  public TimerController(Connection connection) {
+    this.connection = connection;
+  }
+
+  public String startTimer(String userId) {
+    String eventId = "";
+    Statement statement = null;
+
+    try {
+      statement = connection.createStatement();
+
+      String query = "select * from final table" +
+        "(insert into EVENT " +
+        "(USER_ID, START) " +
+        "values (" + userId + ", current_timestamp ))";
+      ResultSet resultSet = statement.executeQuery(query);
+
+      while (resultSet.next()) {
+        eventId = resultSet.getString("id");
+      }
+    } catch (SQLException sqlException) {
+      sqlException.printStackTrace();
+    }
+
+    return eventId;
+  }
+
+  public void endTimer(String eventId) {
+    Statement statement;
+
+    try {
+      statement = connection.createStatement();
+
+      String query = "update event " +
+        "set END = current_timestamp " +
+        "where id = " + eventId;
+      statement.execute(query);
+    } catch (SQLException sqlException) {
+      sqlException.printStackTrace();
+    }
+  }
+}
--- src/main/java/edu/louisville/cse640/rimer/gtd_timer/Timer.java
+++ src/main/java/edu/louisville/cse640/rimer/gtd_timer/Timer.java
@@ -1,59 +1,65 @@
-//package edu.louisville.cse640.rimer.gtd_timer;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.sql.SQLException;
-//
-//@WebServlet(value = "/Timer")
-//public class Timer extends HttpServlet {
-//
-//  private String userId;
-//  private String username;
-//  private String eventId;
-//
-//  @Override
-//  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//    populateSharedAttributes(req, resp);
-//    try {
-//      eventId = new DB2Controller().startTimer(userId);
-//    } catch (SQLException exception) {
-//      exception.printStackTrace();
-//    }
-//
-//    reloadTimerPage(req, resp);
-//  }
-//
-//  @Override
-//  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//    populateSharedAttributes(req, resp);
-//
-//    try {
-//      new DB2Controller().endTimer(eventId);
-//      eventId = null;
-//    } catch (SQLException exception) {
-//      exception.printStackTrace();
-//    }
-//
-//    reloadTimerPage(req, resp);
-//  }
-//
-//  private void populateSharedAttributes(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//    userId = req.getParameter("userId");
-//    username = req.getParameter("username");
-//    eventId = req.getParameter("eventId");
-//  }
-//
-//  private void reloadTimerPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//    req.setAttribute("eventId", eventId);
-//    req.setAttribute("userId", userId);
-//    req.setAttribute("username", username);
-//    req
-//      .getRequestDispatcher("/WEB-INF/timer.jsp")
-//      .forward(req, resp);
-//  }
-//}
-//
+package edu.louisville.cse640.rimer.gtd_timer;
+
+import edu.louisville.cse640.rimer.controllers.ConnectionPool;
+import edu.louisville.cse640.rimer.controllers.TimerController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(value = "/Timer")
+public class Timer extends HttpServlet {
+  private static Connection connection = null;
+
+  private String userId;
+  private String username;
+  private String eventId;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    populateSharedAttributes(req, resp);
+    if (connection != null) {
+      TimerController timerController = new TimerController(connection);
+      eventId = timerController.startTimer(userId);
+    } else {
+      System.err.println("Connection is null in Timer Servlet");
+    }
+    reloadTimerPage(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    populateSharedAttributes(req, resp);
+    TimerController timerController = new TimerController(connection);
+    if (connection != null) {
+      timerController.endTimer(eventId);
+      eventId = null;
+    } else {
+      System.err.println("Connection is null in Timer Servlet");
+    }
+    reloadTimerPage(req, resp);
+  }
+
+  private void populateSharedAttributes(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    userId = req.getParameter("userId");
+    username = req.getParameter("username");
+    eventId = req.getParameter("eventId");
+    ConnectionPool connectionPool = ConnectionPool.getInstance("jdbc/COMPANY");
+    connection = connectionPool.getConnection();
+  }
+
+  private void reloadTimerPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.setAttribute("eventId", eventId);
+    req.setAttribute("userId", userId);
+    req.setAttribute("username", username);
+    req
+      .getRequestDispatcher("/WEB-INF/timer.jsp")
+      .forward(req, resp);
+  }
+}
+
